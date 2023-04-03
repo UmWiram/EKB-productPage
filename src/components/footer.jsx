fc3c8db433b7d6160675ca1c8162040ebc45b497
wiram <williamumkorea@hotmail.com>
2023-04-03 22:43:12 +0900

    setup footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,6 +3,8 @@ import { IconContext } from "react-icons";
 import { BsInstagram, BsTwitter, BsYoutube} from "react-icons/bs"
 
 const Footer = () => {
+    const year = new Date().getFullYear();
+
     return (
       <div className="footer">
         <div className="about">
@@ -10,11 +12,11 @@ const Footer = () => {
         <p className="about">Kitty Boards is here to bring you some awesome keyboards, ergo keyboards and much more!</p>
         <div className="links">
             <ul>
-                <li>Terms & Conditions</li>
-                <li>Return Policy</li>
-                <li>Contact Us</li>
-                <li>Terms of Service</li>
-                <li>Refund Policy</li>
+                <li><a className="nav-link">Terms & Contidions</a></li>
+                <li><a className="nav-link">Return Policy</a></li>
+                <li><a className="nav-link">Contact Us</a></li>
+                <li><a className="nav-link">Terms of Service</a></li>
+                <li><a className="nav-link">Refund Policy</a></li>
             </ul>
         </div>
         </div>
@@ -22,16 +24,16 @@ const Footer = () => {
             <p className="footer-title">Follow Us</p>
             <IconContext.Provider value={{ size: '1.7em', className: 'react-icons'}}>
                 <div className="icons">
-                    <BsInstagram />
-                    <BsTwitter />
-                    <BsYoutube />
+                    <a href="#"><BsInstagram /></a>
+                    <a href="#"><BsTwitter /></a>
+                    <a href="#"><BsYoutube /></a> 
                 </div>
             </IconContext.Provider>
         <div className="footer-logo">
-            <img src="../public/images/logo.png" alt="kitty board logo" className="logo" />
+            <img src="../images/logo.png" alt="kitty board logo" className="logo" />
         </div>
         <div className="copyright">
-            Copyright&copy; 2023 by Kitty Boards, Inc.
+            Copyright&copy; {year} by Kitty Boards, Inc.
             All rights reserved.
         </div>
         </div>
