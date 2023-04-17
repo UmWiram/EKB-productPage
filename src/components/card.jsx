5dfb8e86141c1b53360a8660895c70ac23479aae
wiram <williamumkorea@hotmail.com>
2023-04-18 03:44:57 +0900

    updated gallery page with different products and configure prop

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,6 +3,7 @@ import React from "react";
 export default function Card(props) {
     return(
         <div className="card">
+            {props.stock === 0 && <div className="card--badge">Sold Out</div>}
             <img src={`../images/${props.img}`} className="card-image" />
             <p className="card-title">{props.title}</p>
             <p className="price">{props.price}</p>
